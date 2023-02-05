7ba41740b3c06b81dd9b0032e78ab2114f431d5e
charliegallop <c.gallop@protonmail.com>
2023-02-05 20:01:11 +1300

    tailwind and skeleton setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-rocket.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+	import { AppBar, AppShell, LightSwitch } from '@skeletonlabs/skeleton'
+
+</script>
+
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">(lead)</svelte:fragment>
+			(center)
+			<svelte:fragment slot="trail">
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment> -->
+	<!-- <svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment> -->
+	<!-- <svelte:fragment slot="pageHeader">Page Header</svelte:fragment> -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment> -->
+	<!-- <svelte:fragment slot="footer">Footer</svelte:fragment> -->
+</AppShell>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="p-4">
+	<div class="card p-4 flex flex-col justify-center rounded">
+		<p class="py-4">
+			OK. Who put my calculator in Jell-O? Good one. But uh, seriously, guys who did this? Seriously
+			guys, who did this? I need to know who put my calculator in Jell-O, or I'm gonna lose my
+			freaking mind! OK. Who put my calculator in Jell-O? Good one. But uh, seriously, guys who did this? Seriously
+			guys, who did this? I need to know who put my calculator in Jell-O, or I'm gonna lose my
+			freaking mind!
+		</p>
+		<button class="btn variant-filled-primary btn-base"> SIGN UP </button>
+	</div>
+</div>
