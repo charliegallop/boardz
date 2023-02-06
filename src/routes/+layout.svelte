df9464aeacecc80b223b3466b72cba17017556ba
charliegallop <c.gallop@protonmail.com>
2023-02-06 15:04:08 +1300

    onboarding ui initial design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,8 +9,10 @@
 
 <AppShell>
 	<svelte:fragment slot="header">
-		<AppBar>
-			<svelte:fragment slot="lead">boardz</svelte:fragment>
+		<AppBar class="p-2">
+			<svelte:fragment slot="lead">
+				<a class="hover:brightness-[150%]" href="/">bordz</a> 
+			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<LightSwitch />
 			</svelte:fragment>
@@ -20,8 +22,10 @@
 	<!-- <svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment> -->
 	<!-- <svelte:fragment slot="pageHeader">Page Header</svelte:fragment> -->
 	<!-- Router Slot -->
-	<slot />
-	<!-- ---- / ---- -->
+	<div class="p-4 flex flex-col items-center">
+		<slot />
+	</div>
+		<!-- ---- / ---- -->
 	<!-- <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment> -->
 	<!-- <svelte:fragment slot="footer">Footer</svelte:fragment> -->
 </AppShell>
